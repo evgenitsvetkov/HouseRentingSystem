d469bf1ea2a72d4d9fe06aa58a6752b0cd91a8d3
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-03-09 21:33:03 +0200

    Agent service implemented

--- HouseRentingSystem.Core/Services/AgentService.cs
+++ HouseRentingSystem.Core/Services/AgentService.cs
@@ -14,9 +14,15 @@ namespace HouseRentingSystem.Core.Services
             repository = _repository;
         }
 
-        public Task CreateAsync(string userId, string phoneNumber)
+        public async Task CreateAsync(string userId, string phoneNumber)
         {
-            throw new NotImplementedException();
+            await repository.AddAsync(new Agent() 
+            {
+                UserId = userId,
+                PhoneNumber = phoneNumber
+            });
+
+            await repository.SaveChangesAsync();
         }
 
         public async Task<bool> ExistByIdAsync(string userId)
@@ -25,14 +31,16 @@ namespace HouseRentingSystem.Core.Services
                 .AnyAsync(a => a.UserId == userId);
         }
 
-        public Task<bool> UserHasRentsAsync(string userId)
+        public async Task<bool> UserHasRentsAsync(string userId)
         {
-            throw new NotImplementedException();
+            return await repository.AllReadOnly<House>()
+                 .AnyAsync(h => h.RenterId == userId);
         }
 
-        public Task<bool> UserWithPhoneNumberExistsAsync(string phoneNumber)
+        public async Task<bool> UserWithPhoneNumberExistsAsync(string phoneNumber)
         {
-            throw new NotImplementedException();
+            return await repository.AllReadOnly<Agent>()
+                .AnyAsync(a => a.PhoneNumber == phoneNumber);
         }
     }
 }
